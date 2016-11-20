96782fa56666f1009c9c8a5ad0122b850261c8c5
Giancarlo Hernandez <ghernandez@adasons.com>
2016-11-20 11:58:25 -0400

    Clicker Premium

--- site.js
+++ site.js
@@ -1,12 +1,42 @@
-var boxes = document.getElementsByClassName('click-box')
-var dataClick = 'data-clickc';
-var handleClickCountForBoxes = function(e){
-    var clickCount = parseInt(this.getAttribute(dataClick));
-    clickCount++;
-    var spanText = "<span class='click-box-text'>" + clickCount + "</span>";
-    this.innerHTML = spanText;
-    this.setAttribute(dataClick, clickCount);
-};
-for(var i = 0; i < boxes.length; i++){
-    boxes[i].addEventListener("click", handleClickCountForBoxes, false);
+var dataClick = 'data-id';
+var clickers = [
+    {name: "Green", color: "rgb(179, 210, 52)", clicks: 0},
+    {name: "Orange", color: "#F79420", clicks: 0}
+]
+var ol = document.createElement("ol");
+var cb = document.getElementById("cb");
+var cbn = document.getElementById("cbn");
+var cbt = document.getElementById("cbt");
+
+var handleLisClick = function(){
+    var id = parseInt(this.getAttribute("id"));
+    var clicker = clickers[id];
+    var style = "background-color:" + clicker.color;
+    cb.setAttribute(dataClick, id);
+    cb.setAttribute("style", style);
+    cbn.innerHTML = clicker.name;
+    cbt.innerHTML = clicker.clicks;
+
 }
+var handleClickCountForBox = function(e){
+    var id = parseInt(this.getAttribute(dataClick));
+    var clicker = clickers[id];
+    clicker.clicks++;    
+    cbt.innerHTML = clicker.clicks;
+};
+cb.addEventListener("click", handleClickCountForBox, false);
+
+for(var i = 0; i < clickers.length; i++){
+    var style = 
+    "background-color:" + clickers[i].color + ";" +
+    "cursor: pointer;";
+    var li = document.createElement("li");
+    li.setAttribute("style", style);
+    li.setAttribute("id", i);
+    li.addEventListener("click", handleLisClick, false);
+ol.appendChild(li);
+
+}
+
+var listContainer = document.getElementsByClassName('click-box-list-container')[0];
+listContainer.appendChild(ol);
